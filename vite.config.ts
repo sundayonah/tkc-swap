a3334182f7068761fd02acca1c2b7c0b43e9ebe8
sundayonah <sundayonah94@gmail.com>
2024-05-23 23:35:18 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@web3modal/ethers/react': '@web3modal/ethers5/react' // Adjust if necessary
+    }
+  }
 })
